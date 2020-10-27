d659b224aa556a0cccf8accdca7f1e6415c68778
mcyoloswagham <dynapate@gmail.com>
2020-10-27 14:05:20 +0100

    using new gif crate

--- src/capture.rs
+++ src/capture.rs
@@ -9,7 +9,10 @@ use std::sync::atomic::Ordering;
 use std::time::Duration;
 use winapi::shared::windef;
 use winapi::um::winuser;
-use gif;
+use image::codecs::gif as Igif;
+use image::ImageBuffer;
+use image::Rgba;
+use std::io::Write;
 
 const TIME_SLEEP_FRAME : f64 = 1.0/60.0;
 
@@ -37,8 +40,14 @@ pub unsafe fn start_recording(hwnd : windef::HWND, window_state : &mut WindowSta
         let frame_left = view_area.left + crate::LEFT_EXTEND;
         let frame_top = view_area.top + crate::TOP_EXTEND;
 
-        let frame_width = view_area.right - view_area.left - crate::LEFT_EXTEND - crate::RIGHT_EXTEND;
-        let frame_height = view_area.bottom - view_area.top - crate::TOP_EXTEND - crate::BOTTOM_EXTEND;
+        let frame_width = (view_area.right - view_area.left - crate::LEFT_EXTEND - crate::RIGHT_EXTEND) as u32;
+        let frame_height = (view_area.bottom - view_area.top - crate::TOP_EXTEND - crate::BOTTOM_EXTEND) as u32;
+
+        view_area.left += crate::LEFT_EXTEND;
+        view_area.top += crate::TOP_EXTEND;
+
+        view_area.right -= crate::RIGHT_EXTEND;
+        view_area.bottom -= crate::BOTTOM_EXTEND;
 
         let mut capturer = Capturer::new(0).unwrap();
 
@@ -47,12 +56,20 @@ pub unsafe fn start_recording(hwnd : windef::HWND, window_state : &mut WindowSta
         //unless you capture a frame, unlike the fucking example
         //which tries to get the geometry without having captured 1 frame
         //this is stupid as fuck, send PR upstream
-        capturer.capture_frame().unwrap();
-
+        let num_pixels = capturer.capture_frame().unwrap().len();
         let (cx, cy) = capturer.geometry();
+        let cx = cx as usize;
+        let cy = cy as usize;
+        let rows = (num_pixels / cx) as u32;
+        let cols = (num_pixels / cy) as u32;
 
-        let mut encoder = gif::Encoder::new(&mut buffer_file, frame_width as u16, frame_height as u16, &[]).unwrap();
-        //encoder.set_repeat(gif::Repeat::Infinite).unwrap();
+        println!("rows {} cols {}", rows, cols);
+
+        let mut encoder = Igif::GifEncoder::new(&mut buffer_file);
+        //let mut pixels : Vec<u8> = Vec::with_capacity((cx * cy * 3) as usize);
+
+        let mut frame_count = 0;
+        let mut frames : Vec<image::Frame> = Vec::with_capacity(60);
 
         loop {
 
@@ -60,36 +77,41 @@ pub unsafe fn start_recording(hwnd : windef::HWND, window_state : &mut WindowSta
             if crate::SHOULD_STOP.load(Ordering::Relaxed) {
 
                 //Signal should main thread that all rendering operations have stopped
+                drop(encoder);
                 crate::SHOULD_STOP.store(false, Ordering::Relaxed);
                 println!("we're here fucker");
                 break;
 
             }
 
-            let frame = capturer.capture_frame().unwrap();
+            let frame : Vec<Bgr8> = capturer.capture_frame().unwrap();
+            println!("rect {} {} {} {}", view_area.left, view_area.top, view_area.right, view_area.bottom);
+            let mut index = 0;
+            let new_frame : Vec<u8> = frame.into_iter().filter_map(|pixel| {
 
-            //reorder BGRA8 to RGBA and also flatten into Vec<u8>
-            let mut pixels = Vec::with_capacity((cx * cy * 4) as usize);
+                let offset_in_row = (index % cx) as i32;
+                let num_row = (index / cx) as i32;
+                index += 1;
+                if (view_area.left <= offset_in_row && offset_in_row < view_area.right) && (view_area.top <= num_row && num_row < view_area.bottom) {
+                    let arr : Vec<u8> = vec![pixel.r, pixel.g, pixel.b, pixel.a];
+                    Some(arr)
+                } else {
+                    None
+                }
+            })
+            .flatten()
+            .collect();
 
-            for Bgr8 {b, g, r, a} in frame {
-                pixels.push(r);
-                pixels.push(g);
-                pixels.push(b);
-                pixels.push(a);
+            let image_buffer : ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::from_vec(frame_width, frame_height, new_frame).unwrap();
+            let mut cur_frame = image::Frame::new(image_buffer)
+            frames.push();
+
+            if frame_count == 2 {
+                encoder.encode_frames(frames.clone()).unwrap();
             }
 
-            println!("cx : {}, cy : {}, pixels_len : {}", cx, cy, pixels.len());
+            frame_count += 1;
 
-            let mut gif_frame = gif::Frame::from_rgba_speed(cx as u16, cy as u16, &mut pixels, 10);
-
-            gif_frame.left  =   frame_left as u16;
-            gif_frame.top   =   frame_top as u16;
-            gif_frame.width =   frame_width as u16;
-            gif_frame.left  =   frame_height as u16;
-
-            encoder.write_frame(&gif_frame).unwrap();
-
-            thread::sleep(Duration::from_secs_f64(TIME_SLEEP_FRAME));
         }
 
 
