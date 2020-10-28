e90beeacb41d3fc32cca984e5748394943897585
mcyoloswagham <dynapate@gmail.com>
2020-10-28 18:07:52 +0100

    all gif encoders don't work (image-gif), TODO: switch to RAV1E

--- src/capture.rs
+++ src/capture.rs
@@ -12,7 +12,9 @@ use winapi::um::winuser;
 use image::codecs::gif as Igif;
 use image::ImageBuffer;
 use image::Rgba;
+use gif;
 use std::io::Write;
+use scrap;
 
 const TIME_SLEEP_FRAME : f64 = 1.0/60.0;
 
@@ -34,83 +36,82 @@ pub unsafe fn start_recording(hwnd : windef::HWND, window_state : &mut WindowSta
     thread::spawn(move || {
 
         let mut view_area : windef::RECT = mem::zeroed();
-        winuser::GetClientRect(tcast::<u64, windef::HWND>(fake_hwnd), &mut  view_area);
+        winuser::GetWindowRect(tcast::<u64, windef::HWND>(fake_hwnd), &mut  view_area);
 
         //subtract the fake client area from the actual view area
-        let frame_left = view_area.left + crate::LEFT_EXTEND;
-        let frame_top = view_area.top + crate::TOP_EXTEND;
+        let frame_width = (view_area.right - view_area.left) as u32;
+        let frame_height = (view_area.bottom - view_area.top) as u32;
 
-        let frame_width = (view_area.right - view_area.left - crate::LEFT_EXTEND - crate::RIGHT_EXTEND) as u32;
-        let frame_height = (view_area.bottom - view_area.top - crate::TOP_EXTEND - crate::BOTTOM_EXTEND) as u32;
+        
+        //view_area.left += crate::LEFT_EXTEND;
+        //view_area.top += crate::TOP_EXTEND;
 
-        view_area.left += crate::LEFT_EXTEND;
-        view_area.top += crate::TOP_EXTEND;
+        //view_area.right -= crate::RIGHT_EXTEND;
+        //view_area.bottom -= crate::BOTTOM_EXTEND;
 
-        view_area.right -= crate::RIGHT_EXTEND;
-        view_area.bottom -= crate::BOTTOM_EXTEND;
+        //let mut capturer = Capturer::new(0).unwrap();
+        //let frame_other : Vec<Bgr8> = capturer.capture_frame().unwrap();
+        //println!("frame_other {:?}", frame_other.get(20).unwrap());
+        let mut pd = scrap::Display::primary().unwrap();
+        let mut capt = scrap::Capturer::new(pd).unwrap();
 
-        let mut capturer = Capturer::new(0).unwrap();
+        let cx = capt.width() as u16;
+        let cy = capt.height() as u16;
+        //let rows = (num_pixels / cx) as u32;
+        //let cols = (num_pixels / cy) as u32;
 
-        //Motherfucker retarded code in the example
-        //the capture manager doesn't set it's own width and height
-        //unless you capture a frame, unlike the fucking example
-        //which tries to get the geometry without having captured 1 frame
-        //this is stupid as fuck, send PR upstream
-        let num_pixels = capturer.capture_frame().unwrap().len();
-        let (cx, cy) = capturer.geometry();
-        let cx = cx as usize;
-        let cy = cy as usize;
-        let rows = (num_pixels / cx) as u32;
-        let cols = (num_pixels / cy) as u32;
+        println!("cx {} cy {}", cx, cy);
 
-        println!("rows {} cols {}", rows, cols);
-
-        let mut encoder = Igif::GifEncoder::new(&mut buffer_file);
+        let mut encoder = gif::Encoder::new(&mut buffer_file, cx, cy, &[]).unwrap();
+        //let mut encoder = Igif::GifEncoder::new(&mut buffer_file);
         //let mut pixels : Vec<u8> = Vec::with_capacity((cx * cy * 3) as usize);
 
         let mut frame_count = 0;
-        let mut frames : Vec<image::Frame> = Vec::with_capacity(60);
+        gif::Encoder::set_repeat(&mut encoder, gif::Repeat::Infinite).unwrap();
+        let mut frames : Vec<gif::Frame> = Vec::with_capacity(2);
 
         loop {
 
-            //Atomic bool is true therefore we should stop.
-            if crate::SHOULD_STOP.load(Ordering::Relaxed) {
+            //println!("rect {} {} {} {}", view_area.left, view_area.top, view_area.right, view_area.bottom);
+            let mut frame : Vec<u8> = loop {
+                match capt.frame() {
+                    Ok(frame) => { break frame.to_vec(); },
+                    Err(_) => continue,
+                }
+            };
 
-                //Signal should main thread that all rendering operations have stopped
-                drop(encoder);
-                crate::SHOULD_STOP.store(false, Ordering::Relaxed);
-                println!("we're here fucker");
-                break;
+            println!("sanity3");
 
+            for chunk in frame.chunks_exact_mut(4) {
+                chunk.swap(0,2);
             }
 
-            let frame : Vec<Bgr8> = capturer.capture_frame().unwrap();
-            println!("rect {} {} {} {}", view_area.left, view_area.top, view_area.right, view_area.bottom);
-            let mut index = 0;
-            let new_frame : Vec<u8> = frame.into_iter().filter_map(|pixel| {
+            let mut cur_frame = gif::Frame::from_rgba_speed(cx as u16, cy as u16, &mut frame, 30); 
+            cur_frame.dispose = gif::DisposalMethod::Any;
 
-                let offset_in_row = (index % cx) as i32;
-                let num_row = (index / cx) as i32;
-                index += 1;
-                if (view_area.left <= offset_in_row && offset_in_row < view_area.right) && (view_area.top <= num_row && num_row < view_area.bottom) {
-                    let arr : Vec<u8> = vec![pixel.r, pixel.g, pixel.b, pixel.a];
-                    Some(arr)
-                } else {
-                    None
-                }
-            })
-            .flatten()
-            .collect();
-
-            let image_buffer : ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::from_vec(frame_width, frame_height, new_frame).unwrap();
-            let mut cur_frame = image::Frame::new(image_buffer)
-            frames.push();
+            frames.push(cur_frame);
+            frame_count += 1;
 
             if frame_count == 2 {
-                encoder.encode_frames(frames.clone()).unwrap();
+                println!("lmao {}", frames.len());
+                for gif_frame in &mut frames {
+                    gif_frame.delay = 50;
+                    encoder.write_frame(gif_frame).unwrap();
+                }
+                frame_count = 0;
+                frames.clear();
+
+                //Atomic bool is true therefore we should stop.
+                if crate::SHOULD_STOP.load(Ordering::Relaxed) {
+
+                    crate::SHOULD_STOP.store(false, Ordering::Relaxed);
+                    println!("we're here fucker");
+                    break;
+
+                }
+
             }
 
-            frame_count += 1;
 
         }
 
